83bc38fdbe0c967d731b8abd4fbf8d4574e4423d
Rafael Dias <rafael78@gmail.com>
2014-08-18 23:53:38 -0300

    Revert "desconsiderar"
    This reverts commit dabb97792fee29bbd6ca3b2b3c30d290a14e9b23.

--- geral.php
+++ geral.php
@@ -417,8 +417,7 @@ else 								$status .= "<br>Resultados Extra-Oficiais";
 
 
 
-$txt_especifico = date("d/m/Y  -  H:i:s");
-
+$txt_especifico = date("j/M/Y - G:i:s (T)");
 
 //$txt_especifico .= "<br><br>Resultados Acumulados/Overall Results - Motos/Bikes";
 
